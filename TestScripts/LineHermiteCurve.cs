665e2d127036809cdab4bccf0f88e05b5234b3af
yangpei1010110 <28611200+yangpei1010110@users.noreply.github.com>
2022-11-29 14:52:11 +0800

    Init

---
+++ TestScripts/LineHermiteCurve.cs
@@ -0,0 +1,71 @@
+﻿using Sirenix.OdinInspector;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace TestScripts
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class LineHermiteCurve : MonoBehaviour
+    {
+        private LineRenderer _lineRenderer;
+
+        [LabelText("起始位置")] public Vector3 startPosition;
+        [LabelText("起始速度")] public Vector3 startVelocity;
+        [LabelText("终止位置")] public Vector3 endPosition;
+        [LabelText("终止速度")] public Vector3 endVelocity;
+
+        public int pointCount = 50;
+
+        private void Awake()
+        {
+            _lineRenderer ??= GetComponent<LineRenderer>();
+        }
+
+        private Vector3 HermiteCurve(Vector3 startPos, Vector3 startVelocity, Vector3 endVelocity, Vector3 endPos, float time)
+        {
+            time = math.clamp(time, 0f, 1f);
+
+            Vector3 result0 = (1 - 3 * math.pow(time, 2) + 2 * math.pow(time, 3)) * startPos;
+
+            Vector3 result1 = (time - 2 * math.pow(time, 2) + math.pow(time, 3)) * startVelocity;
+
+            Vector3 result2 = (-math.pow(time, 2) + math.pow(time, 3)) * endVelocity;
+
+            Vector3 result3 = (3 * math.pow(time, 2) - 2 * math.pow(time, 3)) * endPos;
+
+            return result0 + result1 + result2 + result3;
+        }
+
+        private void Update()
+        {
+            Vector3[] tempData = new Vector3[pointCount + 1];
+            float     step     = 1f / pointCount;
+            for (int i = 0; i < tempData.Length; i++)
+            {
+                tempData[i] = HermiteCurve(startPosition, startVelocity, endVelocity, endPosition, step * i);
+            }
+
+            _lineRenderer.positionCount = tempData.Length;
+            _lineRenderer.SetPositions(tempData);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(startPosition, 0.1f);
+            Gizmos.DrawLine(startPosition, startPosition + startVelocity);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(endPosition, 0.1f);
+            Gizmos.DrawLine(endPosition, endPosition + endVelocity);
+
+            if (_lineRenderer != null && _lineRenderer.positionCount > 0)
+            {
+                Gizmos.color = Color.blue;
+                for (int i = 0; i < _lineRenderer.positionCount; i++)
+                {
+                    Gizmos.DrawSphere(_lineRenderer.GetPosition(i), 0.02f);
+                }
+            }
+        }
+    }
+}
---
+++ TestScripts/LineRendererTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using JetBrains.Annotations;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace TestScripts
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class LineRendererTest : MonoBehaviour
+    {
+        private LineRenderer _lineRenderer;
+        public  Vector3[]    data;
+        public  int          pointCount = 10;
+
+        private void Awake()
+        {
+            _lineRenderer ??= GetComponent<LineRenderer>();
+        }
+
+        private float GetY(int j, int i, float x)
+        {
+            if (j == 0)
+            {
+                return data[i - 1].y;
+            }
+
+            float t_ij = data[i + j - 1].x;
+            float t_i  = data[i     - 1].x;
+
+            float t = x;
+
+            return (
+                       (t_ij - t)   * GetY(j - 1, i, x)
+                     + (t    - t_i) * GetY(j - 1, i + 1, x)
+                   )
+                  /
+                   (
+                       t_ij - t_i
+                   );
+        }
+
+        private void Update()
+        {
+            if (data != null && data.Length > 2)
+            {
+                Array.Sort(data, (vl, vr) => (int)math.ceil(math.abs(vl.z - vr.z)));
+                float minX = float.MaxValue;
+                float maxX = float.MinValue;
+                for (int i = 0; i < data.Length; i++)
+                {
+                    minX = math.min(minX, data[i].x);
+                    maxX = math.max(maxX, data[i].x);
+                }
+
+                Vector3[] tempData = new Vector3[pointCount + 1];
+                float     step     = (maxX - minX) / pointCount;
+                for (int i = 0; i < tempData.Length; i++)
+                {
+                    tempData[i].x = minX + step * i;
+                    tempData[i].y = GetY(data.Length - 1, 1, tempData[i].x);
+                }
+
+                _lineRenderer.positionCount = tempData.Length;
+                _lineRenderer.SetPositions(tempData);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (data != null && data.Length > 0)
+            {
+                Gizmos.color = Color.red;
+                for (int i = 0; i < data.Length; i++)
+                {
+                    Gizmos.DrawSphere(data[i], 0.1f);
+                }
+            }
+
+            if (_lineRenderer != null && _lineRenderer.positionCount > 0)
+            {
+                Gizmos.color = Color.blue;
+                for (int i = 0; i < _lineRenderer.positionCount; i++)
+                {
+                    Gizmos.DrawSphere(_lineRenderer.GetPosition(i), 0.02f);
+                }
+            }
+        }
+    }
+}
